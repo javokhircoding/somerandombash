7c87aa51fd0838ffbd7321805e3b580e76e79e21
Javoxir Shermatov <javokhir.coding@gmail.com>
2025-08-31 23:07:43 +0500

    fixwhile

--- while.sh
+++ while.sh
@@ -8,3 +8,11 @@ do
     (( myvar= $myvar +1 ))
     sleep 0.5
 done
+
+
+while [ -f todo.sh ]
+do
+    echo "Test file exists. Date: $(date)"
+done
+    
+echo "The file no longer exists. Date: $(date)"
